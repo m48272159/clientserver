8d4f6a5ac0a6dd2a7794e2a80bf38775d1219428
mohan <m48272159@gmail.com>
2024-11-15 09:42:31 +0000

    Added options processing

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
 .PHONY: all docs clean rebuild
 
+BOOST_ROOT ?= $(HOME)/software/boost
+
 SOURCES:= tcpapplication.cpp
 TARGET:= tcpapplication
 DOCSDIR:= htmldocs
 CC:= g++
 CPPFLAGS:= -std=c++11
 
+# -lboost_program_options HAS to be be placed after sources
 $(TARGET):$(SOURCES)
-	$(CC) $(CPPFLAGS) $< -o $@
+	$(CC) $(CPPFLAGS) -I$(BOOST_ROOT)/include -L$(BOOST_ROOT)/lib $< -lboost_program_options -o $@
 
 all:$(TARGET) docs
 
--- tcpapplication.cpp
+++ tcpapplication.cpp
@@ -1,13 +1,57 @@
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 #include <iostream>
 
-#include <boost/asio.hpp>
+namespace opts = boost::program_options;
 
-// TODO: Read the documentation at https://www.boost.org/doc/libs/1_86_0/doc/html/boost_asio/tutorial.html ad implement!
+constexpr int MIN_NONPRIVILEGED_PORT = 1024;
 
-
-int main ()
+namespace
 {
-	std::cout << "Hello World\n" << std::endl;
-	return 0;
-}
+bool Validate(const opts::variables_map &vm)
+{
+    bool bValid = true;
+    if ((vm.count("client") > 0) && (vm.count("server") > 0))
+    {
+        bValid = false;
+        std::cerr << "Please start either the server or client. " << std::endl;
+    }
 
+    const int port = vm["port"].as<int>();
+    if (port < 1024)
+    {
+        bValid = false;
+        std::cerr << "Cannot start server with port less than 1024. Value "
+                     "provided: "
+                  << port << std::endl;
+    }
+    return bValid;
+}
+} // namespace
+
+int main(int argc, char **argv)
+{
+    int port;
+    opts::options_description options;
+    // clang-format off
+    options.add_options()
+	   ("server", "Start server")
+	   ("client", "Start client")
+	   ("help", "usage")
+	   ("port", opts::value<int>(&port)->default_value(MIN_NONPRIVILEGED_PORT + 1), "Server port")
+	   ;
+    // clang-format on
+
+    opts::variables_map vm;
+    opts::store(opts::parse_command_line(argc, argv, options), vm);
+    opts::notify(vm);
+
+    if (!Validate(vm))
+    {
+        return -1;
+    }
+
+    // Create server or client as needed
+
+    return 0;
+}
