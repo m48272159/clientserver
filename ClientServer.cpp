504507c28ca6d900c01b3daad31fbfbbcec9d111
mohan <m48272159@gmail.com>
2024-11-19 11:31:09 +0000

    Fixed socket read issue - move vs ref

--- ClientServer.cpp
+++ ClientServer.cpp
@@ -1,13 +1,17 @@
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
 #include <iostream>
-#include <thread>
 #include <string>
+#include <thread>
 
+// TODO: Avoid hardcoding of server IP address
+const auto SERVERADDR{boost::asio::ip::address_v4::loopback()};
 
 // Function to handle client connections
-void HandleClient(boost::asio::ip::tcp::socket& socket) {
-    try {
+void HandleClient(boost::asio::ip::tcp::socket socket)
+{
+    try
+    {
         // Read data from the client
         boost::asio::streambuf buffer;
         boost::asio::read_until(socket, buffer, '\n');
@@ -17,63 +21,75 @@ void HandleClient(boost::asio::ip::tcp::socket& socket) {
 
         // Echo the data to the console
         std::cout << "Received from client: " << data << std::endl;
-    } catch (const boost::system::system_error& e) {
+    }
+    catch (const boost::system::system_error &e)
+    {
         std::cerr << "Error handling client: " << e.what() << std::endl;
     }
 }
 
 // Function to start the server
-void StartServer(unsigned short port) {
-	// Create an IO service
+void StartServer(unsigned short port)
+{
+    // Create an IO service
     boost::asio::io_service ioService;
     // Create a TCP endpoint
-    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address_v4::any(), port);
+    boost::asio::ip::tcp::endpoint endpoint(SERVERADDR, port);
 
     // Create a TCP acceptor
     boost::asio::ip::tcp::acceptor acceptor(ioService, endpoint);
 
+    std::cout << "### Started server at " << "IP: " << endpoint.address() << "\t" << "Port: " << endpoint.port()
+              << std::endl;
+
     // Start accepting clients
-    while (true) {
-        try {
+    while (true)
+    {
+        try
+        {
             // Create a new socket for the client
             boost::asio::ip::tcp::socket socket(ioService);
-
             // Accept the client connection
             acceptor.accept(socket);
-
+            std::cout << "### Client connected" << std::endl;
             // Spawn a new thread to handle the client
-            std::thread clientThread(HandleClient, std::ref(socket));
+            std::thread clientThread(HandleClient, std::move(socket));
             clientThread.detach();
-        } catch (const boost::system::system_error& e) {
+        }
+        catch (const boost::system::system_error &e)
+        {
             std::cerr << "Error accepting client: " << e.what() << std::endl;
         }
     }
 }
 
-
 // Function to handle user input
-void HandleUserInput(boost::asio::ip::tcp::socket& socket) {
-    try {
+void HandleUserInput(boost::asio::ip::tcp::socket &socket)
+{
+    try
+    {
         // Read user input
         std::string userInput;
         std::getline(std::cin, userInput);
 
         // Send the user input to the server
         boost::asio::write(socket, boost::asio::buffer(userInput + "\n"));
-    } catch (const boost::system::system_error& e) {
+    }
+    catch (const boost::system::system_error &e)
+    {
         std::cerr << "Error sending data to server: " << e.what() << std::endl;
     }
 }
 
 void StartClient(unsigned short port)
 {
-    try {
+    try
+    {
         // Create an IO service
         boost::asio::io_service ioService;
 
         // Create a TCP endpoint
-		// TODO: remove hardcoding of loopback address
-        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address_v4::loopback(), port);
+        boost::asio::ip::tcp::endpoint endpoint(SERVERADDR, port);
 
         // Create a TCP socket
         boost::asio::ip::tcp::socket socket(ioService);
@@ -82,18 +98,21 @@ void StartClient(unsigned short port)
         socket.connect(endpoint);
 
         // Start a loop to handle user input
-        while (true) {
+        while (true)
+        {
             // Handle user input
             HandleUserInput(socket);
 
             // Check if the user wants to exit
-            if (std::cin.eof()) {
+            if (std::cin.eof())
+            {
                 break;
             }
         }
-    } catch (const boost::system::system_error& e) {
+    }
+    catch (const boost::system::system_error &e)
+    {
         std::cerr << "Error connecting to server: " << e.what() << std::endl;
-		exit(1);
+        exit(1);
     }
 }
-
